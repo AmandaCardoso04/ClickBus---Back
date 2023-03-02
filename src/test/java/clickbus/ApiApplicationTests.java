087da2b68de3c0de7a26abb501a8fe8266f508d3
AmandaCardoso04 <manda04ae@gmail.com>
2023-03-02 13:32:57 -0300

    Inicio

---
+++ src/test/java/clickbus/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package clickbus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
